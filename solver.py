8104f1268d69a2173dc76ac0c896b5c2cc6b1b46
Areli Jimenez Huerta <areli.jimenez.huerta@gmail.com>
2017-07-16 22:55:49 -0700

    adding strategies and sequences to solver

--- solver.py
+++ solver.py
@@ -1,9 +1,45 @@
 
 class Solver:
+	stages = {
+		'north_cross':[
+			'rotate_top_edges',
+			'flip_top_edge',
+		],
+		'north_corners':[
+			'top_corner_down',
+			'bottom_corner_up',
+		],
+		'middle_layer':[
+			'top_edge_to_right',
+			'top_edge_to_left',
+		],
+		'south_edges':[
+			'make_bend',
+			'make_line',
+			'make_bend',
+		],
+		'south_corners':[
+			'toggle_corners',
+			'swap_corners',
+		],
+		'south_edges':[
+			'efgh_clockwise',
+			'efgh_counter',
+		],
+	}
 	sequences = {
-		'rotate_top_edges': ['l','l','r','r','D','D','L','L','R','R'],
-		'flip_edge': ['r','U','f','u'],
-		'bottom_corner_up': ['r','d','R','D'],
+		'rotate_top_edges':  ['l','l','r','r','D','D','L','L','R','R'],
+		'flip_top_edge':     ['r','U','f','u'],
+		'bottom_corner_up':  ['r','d','R','D'],
+		'top_corner_down':   ['r','d','R'],
+		'top_edge_to_right': ['U','R','u','r','u','f','U','F'],
+		'top_edge_to_left':  ['u','l','U','L','U','F','u','f'],
+		'make_bend':         ['F','U','R','u','r','f'],
+		'make_line':         ['F','R','U','r','u','f'],
+		'toggle_corners':    ['R','U','r','U','R','u','u','r'],
+		'swap_corners':      ['r','F','r','B','B','R','f','r','B','B','R','R','u'],
+		'efgh_clockwise':    ['F','F','U','L','r','F','F','l','R','U','F','F'],
+		'efgh_counter':      ['F','F','u','L','r','F','F','l','R','u','F','F'],
 	}
 	def __init__(self, cube, queue):
 		self.block_queue = []

24f64549778453826a933c8f1524a09b6d40a38c
D. Casey Tucker <dcaseytucker@gmail.com>
2017-08-18 23:24:32 -0700

    adding moon texture, input iata code to select source airport

--- earth.py
+++ earth.py
@@ -6,35 +6,11 @@ import nvector as nv
 from visual import *
 
 def keydown(evt):
-	keymap = {
-		'r': 'U',
-		'u': 'u',
-		't': 'U',
-		'y': 'u',
-		'b': 'd',
-		'v': 'd',
-		's': 'd',
-		'l': 'd',
-		'n': 'D',
-		'e': 'l',
-		'd': 'L',
-		'i': 'R',
-		'k': 'r',
-		'f': 'f',
-		'j': 'F',
-		'g': 'B',
-		'h': 'b',
-	}
+	global iata_code
 	k = evt.key
 
-	if k in keymap:
-		solver.queue.insert(0, keymap[k])
-		return
-
-	if k in ('esc','Q','q'):
+	if k == 'esc':
 		exit()
-	#elif k.upper() in ('R','L','U','D','B','F'):
-	#	solver.queue.insert(0,k)
 	elif k == 'left':
 		camera.d_theta_x = radians(-2)
 	elif k == 'right':
@@ -47,19 +23,10 @@ def keydown(evt):
 		camera.d_theta_z = radians(-0.5)
 	elif k == 'page down':
 		camera.d_theta_z = radians(0.5)
-	elif k == ',':
-		select_block((-1,0,0))
-	elif k == '.':
-		select_block((1,0,0))
-	elif k == ';':
-		select_block((0,-1,0))
-	elif k == "'":
-		select_block((0,1,0))
-	elif k == "[":
-		select_block((0,0,-1))
-	elif k == "]":
-		select_block((0,0,1))
-
+	elif (k >= 'A' and k <= 'Z') or (k >='a' and k <='z'):
+		iata_code += k
+	elif k == '\n':
+		load_iata()
 	else:
 		print vars(evt)
 
@@ -79,6 +46,7 @@ def mousemove(evt):
 sun_radius = 695700
 e_distance = 149597870.7
 e_radius = 6371
+moon_earth_distance = 384400
 
 scene = display( title="Earth", width=1200, height=800, background=(0.0,0.0,0.0) )
 #scene.autoscale = False
@@ -97,8 +65,13 @@ scene.bind('keyup'  , keyup)
 scene.bind('mousemove', mousemove)
 
 #sun = sphere( pos=(e_distance,0,0), radius=sun_radius, material=materials.emissive, color=(1,1,1) )
-im = Image.open('Day_lrg.jpg')
-im = im.resize((2048,2048), Image.ANTIALIAS)
+im = Image.open('moonmap4k.jpg')
+im = im.resize((4096,2048), Image.ANTIALIAS)
+moon_tex = materials.texture(data=im, mapping="spherical")
+moon = sphere( pos=(moon_earth_distance, 0, 0), radius=1738, material=moon_tex )
+
+im = Image.open('PathfinderMap.jpg')
+#im = im.resize((2048,2048), Image.ANTIALIAS)
 earth_tex = materials.texture(data=im, mapping="spherical")
 earth = sphere( pos=(0,0,0), radius=e_radius, material=earth_tex )
 earth.rotate( angle=radians(90), axis=(0,1,0), origin=earth.pos )
@@ -121,7 +94,7 @@ def poi( latitude, longitude, altitude=0 ):
 	return pos
 
 def poi_sphere( latitude, longitude, color=(1,1,0) ):
-	s = sphere( pos=poi(latitude, longitude), color=color, radius=50 )
+	s = sphere( pos=poi(latitude, longitude), color=color, radius=50, opacity=0.3 )
 	s.longitude = longitude
 	s.latitude = latitude
 	return s
@@ -132,44 +105,60 @@ def arc( poi1, poi2 ):
 	gp1 = frame_E.GeoPoint(latitude=poi1.latitude, longitude=poi1.longitude, degrees=True)
 	gp2 = frame_E.GeoPoint(latitude=poi2.latitude, longitude=poi2.longitude, degrees=True)
 
+	distance, _azia, _azib = gp1.distance_and_azimuth(gp2)
+	distance = int(distance/50000)
 	path = nv.GeoPath(gp1, gp2)
-	for i in range(0, 21):
-		geo = path.interpolate(0.05*i).to_geo_point()
+	for i in range(0, distance+1):
+		geo = path.interpolate((1.0/distance)*i).to_geo_point()
 		points += [ poi( geo.latitude_deg, geo.longitude_deg, 10 ) ]
 	return points
 
 db = records.Database('mysql://localhost/lang')
 
-source_iata_code = 'NRT'
-source_airport = db.query("""
-	SELECT latitude, longitude, iata_code, country_name
-	FROM airports
-	WHERE iata_code='%s'
-""" % (source_iata_code))[0]
+def load_iata():
+	global shapes, iata_code
+	code = ''+iata_code
+	iata_code = ''
+	source_airport = db.query("""
+		SELECT latitude, longitude, iata_code, country_name
+		FROM airports
+		WHERE iata_code='%s'
+	""" % (code))[0]
 
-routes = db.query("""
-	SELECT DISTINCT latitude, longitude, country_name, iata_code, airports.city_name
-	FROM routes
-	INNER JOIN airports on airports.iata_code = routes.destination_airport_code
-	WHERE source_airport_code='%s'
-	-- AND airline_code='DL'
-""" % (source_airport.iata_code))
+	routes = db.query("""
+		SELECT DISTINCT latitude, longitude, country_name, iata_code, airports.city_name
+		FROM routes
+		INNER JOIN airports on airports.iata_code = routes.destination_airport_code
+		WHERE source_airport_code='%s'
+		-- AND airline_code='DL'
+	""" % (code))
 
-source = poi_sphere(source_airport.latitude, source_airport.longitude)
-for r in routes:
-	if source_airport.country_name != r.country_name:
-		color=(1,1,0)
-	else:
-		color=(0,1,1)
-	dest = poi_sphere( r.latitude, r.longitude, color=color )
-	curve( pos=arc( source, dest ), color=color, radius=20 )
-	label( pos=dest.pos, text=r.city_name, box=False, line=False, opacity=0, yoffset=10, space=20 )
-	print r.latitude, r.longitude
+	for s in shapes:
+		s.visible = False
+		del s
+
+	shapes = []
+	source = poi_sphere(source_airport.latitude, source_airport.longitude)
+	shapes += [source]
+	for r in routes:
+		if source_airport.country_name != r.country_name:
+			color=(1,1,0)
+		else:
+			color=(0,1,1)
+		dest = poi_sphere( r.latitude, r.longitude, color=color )
+		waypoints = points( pos=arc( source, dest ), color=color )
+		dest_label = label( pos=dest.pos, text=r.city_name+" - "+r.iata_code, box=False, line=False, opacity=0, yoffset=10, space=20 )
+		print r.latitude, r.longitude
+		shapes += [ dest, waypoints, dest_label ]
+	return shapes
 
 
 scene.center = (0,0,0)
 #scene.range = (4*e_distance, 4*e_distance, 4*e_distance)
 scene.range = (2*e_radius, 2*e_radius, 2*e_radius)
+shapes = []
+iata_code = 'ILM'
+shapes = load_iata()
 
 while 1:
 	rate(60)
